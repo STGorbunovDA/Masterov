e1a4630531ea9a3258dcbd4ba4885226ca462e59
Дмитрий Горбунов <bous07@mail.ru>
2025-09-28 16:08:41 +0300

    Изменение метода получения заказчиков по имени

--- Masterov.Storage/Storages/Masterov/Customer/GetCustomerByNameStorage.cs
+++ Masterov.Storage/Storages/Masterov/Customer/GetCustomerByNameStorage.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using AutoMapper.QueryableExtensions;
+using Masterov.Domain.Extension;
 using Masterov.Domain.Masterov.Customer.GetCustomerByName;
 using Masterov.Domain.Models;
 using Microsoft.EntityFrameworkCore;
@@ -9,27 +9,29 @@ namespace Masterov.Storage.Storages.Masterov.Customer;
 
 internal class GetCustomerByNameStorage(MasterovDbContext dbContext, IMemoryCache memoryCache, IMapper mapper) : IGetCustomerByNameStorage
 {
-    public async Task<CustomerDomain?> GetCustomerByName(string customerName, CancellationToken cancellationToken)=>
-        (await memoryCache.GetOrCreateAsync<CustomerDomain?>( 
-            nameof(GetCustomerByName),
-            async entry =>
-            {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(1);
+    public async Task<CustomerDomain?> GetCustomerByName(string customerName, CancellationToken cancellationToken)
+    {
+        var cacheKey = $"GetCustomerByName_{customerName}";
+        
+        return (await memoryCache.GetOrCreateAsync(cacheKey, async entry =>
+        {
+            entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(CacheSettings.CacheSeconds);
+
+            var customer = await dbContext.Customers
+                .AsNoTracking() 
+                    .Include(c => c.Orders)
+                        .ThenInclude(o => o.Payments)
+                        .ThenInclude(p => p.Customer)
+                    .Include(c => c.Orders)
+                        .ThenInclude(o => o.Components)
+                        .ThenInclude(pc => pc.ProductType)
+                    .Include(c => c.Orders)
+                        .ThenInclude(o => o.Components)
+                        .ThenInclude(pc => pc.Warehouse)
+                .Where(f => f.Name.ToLower() == customerName.ToLower().Trim())
+                .FirstOrDefaultAsync(cancellationToken);
                 
-                var customer = await dbContext.Customers
-                    .AsNoTracking() 
-                        .Include(c => c.Orders)
-                            .ThenInclude(o => o.Payments)
-                            .ThenInclude(p => p.Customer)
-                        .Include(c => c.Orders)
-                            .ThenInclude(o => o.Components)
-                            .ThenInclude(pc => pc.ProductType)
-                        .Include(c => c.Orders)
-                            .ThenInclude(o => o.Components)
-                            .ThenInclude(pc => pc.Warehouse)
-                    .Where(f => f.Name.ToLower() == customerName.ToLower().Trim())
-                    .FirstOrDefaultAsync(cancellationToken);
-                
-                return mapper.Map<CustomerDomain>(customer);
-            }))!;
+            return mapper.Map<CustomerDomain>(customer);
+        }));
+    }
 }
