709319dda4d17b05bfa79d59dc1bfe82bd2479a0
Дмитрий Горбунов <bous07@mail.ru>
2025-11-03 19:32:14 +0300

    Убрал кеш из метода получения склада по id

--- Masterov.Storage/Storages/Masterov/Warehouse/GetWarehouseByIdStorage.cs
+++ Masterov.Storage/Storages/Masterov/Warehouse/GetWarehouseByIdStorage.cs
@@ -3,22 +3,15 @@ using AutoMapper.QueryableExtensions;
 using Masterov.Domain.Masterov.Warehouse.GetWarehouseById;
 using Masterov.Domain.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace Masterov.Storage.Storages.Masterov.Warehouse;
 
-internal class GetWarehouseByIdStorage(MasterovDbContext dbContext, IMemoryCache memoryCache, IMapper mapper) : IGetWarehouseByIdStorage
+internal class GetWarehouseByIdStorage(MasterovDbContext dbContext, IMapper mapper) : IGetWarehouseByIdStorage
 {
-    public async Task<WarehouseDomain?> GetWarehouseById(Guid warehouseById, CancellationToken cancellationToken) =>
-        (await memoryCache.GetOrCreateAsync<WarehouseDomain?>( 
-            nameof(GetWarehouseById),
-            entry =>
-            {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(1);
-                return dbContext.Warehouses
-                    .AsNoTracking() 
-                    .Where(f => f.WarehouseId == warehouseById)
-                    .ProjectTo<WarehouseDomain>(mapper.ConfigurationProvider)
-                    .FirstOrDefaultAsync(cancellationToken);
-            }))!;
+    public async Task<WarehouseDomain?> GetWarehouseById(Guid warehouseId, CancellationToken cancellationToken) =>
+        await dbContext.Warehouses
+            .AsNoTracking()
+            .Where(f => f.WarehouseId == warehouseId)
+            .ProjectTo<WarehouseDomain>(mapper.ConfigurationProvider)
+            .FirstOrDefaultAsync(cancellationToken);
 }
